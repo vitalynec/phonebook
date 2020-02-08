c0a325a84cf44db7cd29af9ca8f98b08b570b4e8
Vitaly Nechitaylo <v.nechitaylo@cdek.ru>
2020-02-08 18:31:15 +0700

    other

--- src/main/java/com/vitalynec/phonebook/PhonebookApplication.java
+++ src/main/java/com/vitalynec/phonebook/PhonebookApplication.java
@@ -1,13 +1,27 @@
 package com.vitalynec.phonebook;
 
+import com.vitalynec.phonebook.controller.CLIController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhonebookApplication {
+public class PhonebookApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhonebookApplication.class, args);
-	}
+    private static Logger LOG = LoggerFactory.getLogger(PhonebookApplication.class);
 
+    public static void main(String[] args) {
+        LOG.info("STARTING THE APPLICATION");
+        SpringApplication.run(PhonebookApplication.class, args);
+        LOG.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("EXECUTING : command line runner");
+        CLIController controller = new CLIController();
+        controller.getAllUsers();
+    }
 }
