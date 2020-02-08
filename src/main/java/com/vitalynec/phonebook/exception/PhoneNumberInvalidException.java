281411d54bd68b7f5b76d6c16f29213ea1a28749
Vitaly Nechitaylo <v.nechitaylo@cdek.ru>
2020-02-08 18:29:44 +0700

    exception

---
+++ src/main/java/com/vitalynec/phonebook/exception/PhoneNumberInvalidException.java
@@ -0,0 +1,7 @@
+package com.vitalynec.phonebook.exception;
+
+public class PhoneNumberInvalidException extends Exception {
+    public PhoneNumberInvalidException(String message) {
+        super(message);
+    }
+}
